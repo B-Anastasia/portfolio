b48493d0fe231a44667dcc636f062560e0ecd3b8
Anastasia <boshniak.a@gmail.com>
2020-09-25 19:33:36 +0300

    Updates

---
+++ precache-manifest.4ae6e178f64a9caa227f8e0fc1bd0f1e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e5abded0ff82ec1d9061d711945bb7c",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "a87ff3b00664f18e56e7",
+    "url": "/portfolio/static/css/main.ba314a4c.chunk.css"
+  },
+  {
+    "revision": "06d283097b48e4e0d3bf",
+    "url": "/portfolio/static/js/2.8659e357.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.8659e357.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a87ff3b00664f18e56e7",
+    "url": "/portfolio/static/js/main.0eee0390.chunk.js"
+  },
+  {
+    "revision": "8c78779c98e297eec2a1",
+    "url": "/portfolio/static/js/runtime-main.23e48518.js"
+  }
+]);
